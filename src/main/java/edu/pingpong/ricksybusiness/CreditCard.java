8f30db0f958041fc72cead2be2ace921cf99d53e
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-03-03 12:29:49 +0100

    perf: :zap: Preparing Project Structure

--- src/main/java/edu/pingpong/ricksybusiness/CreditCard.java
+++ src/main/java/edu/pingpong/ricksybusiness/CreditCard.java
@@ -4,10 +4,16 @@ public class CreditCard {
 
     private String owner = "";
     private String number = "";
+    private Double credit = 0d;
+    private final String SYMBOL = "EZI";
 
     public CreditCard(String owner, String number) {
         this.number = number;
         this.owner = owner;
+        /* La D recordemos es para señalarle que esun tipo
+        double, pero como la variable "credit" es de tipo
+        Double(que es una clase recordemos), entonces convierte este núero primitivo double en tipo Double*/
+        this.credit = 3000.0d; 
     }
     public String getOwner() {
         return this.owner;
@@ -15,8 +21,12 @@ public class CreditCard {
     public String getNumber() {
         return this.number;
     }
+
+    public Double getCredit() {
+        return this.credit;
+    }
     @Override
     public String toString() {
-        return "Owner: " + this.getOwner() + "number: " + this.getNumber();
+        return "Owner: " + this.getOwner() + "number: " + this.getNumber() + "Credit: " + this.getCredit() + this.SYMBOL;
     }
 }
--- src/test/java/edu/pingpong/ricksybusiness/CreditCardTest.java
+++ src/test/java/edu/pingpong/ricksybusiness/CreditCardTest.java
@@ -2,22 +2,45 @@ package edu.pingpong.ricksybusiness;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CreditCardTest {
+
+    /* Inicializamos la variable que referenciará al objeto 
+    CreditCard */
+    private CreditCard card = null;
+
+    
+    @Before
+    public void setup_card() {
+        /* Tenemos que colocarle el THIS
+        debido a que sino no puede ser usada en el resto
+        de métodos u otras partes de la clase.*/
+        this.card = new CreditCard("Rick", "123456789");
+    }
+
     @Test
     public void constructorTest() {
-        CreditCard card = new CreditCard("Rick", "123456789");
-        assertNotNull(card);
+
+        assertNotNull(this.card);
     }
     @Test
     public void getOwnerTest() {
-        CreditCard card = new CreditCard("Rick", "123456789");
-        assertEquals("Rick", card.getOwner());
+
+        assertEquals("Rick", this.card.getOwner());
     }
     @Test
     public void getNumbeTestr() {
-        CreditCard card = new CreditCard("Rick", "123456789");
-        assertEquals("123456789", card.getNumber());
+
+        assertEquals("123456789", this.card.getNumber());
+    }
+
+    @Test
+    public void getCreditTest() {
+        /* al usar toString no es el toString() que usamos con polimorfismo
+        sino de la clase Double, para convertir ese Double a un String*/
+        assertEquals("3000.0", this.card.getCredit().toString());
     }
 }
