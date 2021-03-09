ec3289ab739235e759d71788d9ff938f48d92893
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-03-09 12:25:46 +0100

    feat: :sparkles: Trying to do getUfoOf() method with streams

--- src/main/java/edu/pingpong/ricksybusiness/domain/UfosPark.java
+++ src/main/java/edu/pingpong/ricksybusiness/domain/UfosPark.java
@@ -1,10 +1,30 @@
 package edu.pingpong.ricksybusiness.domain;
 
+import java.util.*;
+
 public class UfosPark implements GuestDispatcher {
+
+    private double fee = 0d;
+    private final Map<String, String> flota = new HashMap<String, String>();
+
+    UfosPark() {}
+
+    void add(String ovni) {
+        
+		this.flota.putIfAbsent(ovni, null);
+    }
     
     @Override
     public void dispatch(CreditCard creditCard) {
         System.out.println("dispatched");
         
     }
+
+    String getUfoOf(String cardNumber) {
+        
+        String ufo = this.flota.entrySet().stream().map(Map.Entry::getKey );
+        
+    }
+
+
 }
