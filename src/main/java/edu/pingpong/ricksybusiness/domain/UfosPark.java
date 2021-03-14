c7395bd2cca889597c2649d70787bd58383f933b
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-03-14 13:11:05 +0100

    feat: :sparkles: Finished UfosPark Implementation Methods

--- src/main/java/edu/pingpong/ricksybusiness/domain/UfosPark.java
+++ src/main/java/edu/pingpong/ricksybusiness/domain/UfosPark.java
@@ -18,15 +18,31 @@ public class UfosPark implements GuestDispatcher {
     @Override
     public void dispatch(CreditCard creditCard) {
         
-        flota.entrySet().stream().filter(e -> e.getValue() == null).map(entry -> {entry.setValue("54545454545"); return entry;}).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        this.flota.entrySet().stream().filter(e -> e.getValue() == null).map(entry -> {entry.setValue(creditCard.getNumber()); creditCard.pay(fee); return entry;}).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         
+        // this.flota.entrySet().stream().filter(e -> e.getValue().equals(creditCard.getNumber()) && )
     }
 
     String getUfoOf(String cardNumber) {
         
-        Optional<String> ufo = flota.entrySet().stream().filter(e -> e.getValue().equals("4916119711304546")).map(Map.Entry::getKey).map(u -> u.toString()).findFirst();
+        Optional<String> ufo = flota.entrySet().stream().filter(e -> e.getValue().equals(cardNumber)).map(Map.Entry::getKey).map(u -> u.toString()).findFirst();
 
         return (ufo.isPresent()) ? ufo.get() : ufo.orElse("There is not Ufo for this CardNumber");
+    }
+
+    boolean containsCard(String cardNumber) {
+        return this.flota.containsValue(cardNumber);
+    }
+
+    Collection<String> cardNumbers() {
+
+        return this.flota.values();
+    }
+
+    @Override
+    public String toString() {
+        /* %n == \n */
+        return String.format("UFOS: %n %s", flota.keySet().toString());
     }
 
 
